063c001614e9ca256f4f821844f8d48be20240b2
Liam Keegan <liam@keegan.ch>
2021-12-09 13:19:12 +0100

    first attempt

--- build.ps1
+++ build.ps1
@@ -1,9 +1,10 @@
-DEPSDIR=${Env:INSTALL_PREFIX}
-
 echo "SME_DEPS_COMMON_VERSION: ${Env:SME_DEPS_COMMON_VERSION}"
 echo "DUNE_COPASI_VERSION: ${Env:DUNE_COPASI_VERSION}"
+echo "DUNE_VERSION: ${Env:DUNE_VERSION}"
+echo "DUNE_SUPPORT_VERSION: ${Env:DUNE_SUPPORT_VERSION}"
 
 # download common static libs
+
 $client = New-Object System.Net.WebClient
 $client.DownloadFile("https://github.com/spatial-model-editor/sme_deps_common/releases/download/${Env:SME_DEPS_COMMON_VERSION}/sme_deps_common_${Env:OS_TARGET}.tgz", "C:\deps.tgz")
 7z e C:\deps.tgz
@@ -12,33 +13,37 @@ rm tmp.tar
 mv smelibs C:\
 ls C:\smelibs
 
+# function to download, compile & install a dune module
+
 function install_dune($module, $repo, $branch) {
-
-git clone -b $branch --depth 1 https://gitlab.dune-project.org/$repo/dune-$module.git
-cd dune-$module
-mkdir build
-cd build
-cmake -G "Ninja" .. `
-    -DCMAKE_BUILD_TYPE=Release `
-    -DBUILD_SHARED_LIBS=OFF `
-    -DCMAKE_INSTALL_PREFIX="${Env:INSTALL_PREFIX}" `
-    -DCMAKE_PREFIX_PATH="${Env:INSTALL_PREFIX}"
-cmake --build . --parallel
-cmake --install
-cd ../../
-
+    echo "${repo}/${module}/${branch}..."
+    git clone -b ${branch} --depth 1 https://gitlab.dune-project.org/${repo}/dune-${module}.git
+    cd dune-$module
+    mkdir build
+    cd build
+    cmake -G "Ninja" .. `
+        -DCMAKE_BUILD_TYPE=Release `
+        -DBUILD_SHARED_LIBS=OFF `
+        -DCMAKE_INSTALL_PREFIX="${Env:INSTALL_PREFIX}" `
+        -DCMAKE_PREFIX_PATH="${Env:INSTALL_PREFIX}" `
+        -DDUNE_USE_FALLBACK_FILESYSTEM="${Env:DUNE_USE_FALLBACK_FILESYSTEM}"
+    cmake --build . --parallel
+    cmake --install .
+    cd ..\..
 }
 
-install_dune "common" "core" "releases/2.7"
-install_dune "logging" "staging" "releases/2.7"
-install_dune "uggrid" "staging" "releases/2.7"
-install_dune "geometry" "core" "releases/2.7"
-install_dune "grid" "core" "releases/2.7"
-install_dune "localfunctions" "core" "releases/2.7"
-install_dune "istl" "core" "releases/2.7"
-install_dune "typetree" "copasi" "support/dune-copasi-v1.1.0"
-install_dune "functions" "staging" "releases/2.7"
-install_dune "pdelab" "copasi" "support/dune-copasi-v1.1.0"
+# install each dune module in the required order
+
+install_dune "common" "core" "${Env:DUNE_VERSION}"
+install_dune "logging" "staging" "${Env:DUNE_VERSION}"
+install_dune "uggrid" "staging" "${Env:DUNE_VERSION}"
+install_dune "geometry" "core" "${Env:DUNE_VERSION}"
+install_dune "grid" "core" "${Env:DUNE_VERSION}"
+install_dune "localfunctions" "core" "${Env:DUNE_VERSION}"
+install_dune "istl" "core" "${Env:DUNE_VERSION}"
+install_dune "typetree" "copasi" "${Env:DUNE_SUPPORT_VERSION}"
+install_dune "functions" "staging" "${Env:DUNE_VERSION}"
+install_dune "pdelab" "copasi" "${Env:DUNE_SUPPORT_VERSION}"
 install_dune "copasi" "copasi" "${Env:DUNE_COPASI_VERSION}"
 
 mkdir artefacts
