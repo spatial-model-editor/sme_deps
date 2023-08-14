992457aeb5c3b945736979fbd06ce94ff433bfb1
Liam Keegan <liam@keegan.ch>
2023-08-14 16:20:16 +0200

    debug

--- build.sh
+++ build.sh
@@ -2,7 +2,13 @@
 
 set -e -x
 
-#export PATH="/usr/local/opt/llvm/bin:$PATH"
+export PATH="/usr/local/opt/llvm/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/llvm/lib"
+export CPPFLAGS="-I/usr/local/opt/llvm/include"
+
+clang++ --version
+
+clang++ -o x test.cpp -std=c++20 -L/usr/local/opt/llvm/lib/c++ -Wl,-rpath,/usr/local/opt/llvm/lib/c++
 
 /usr/local/opt/llvm/bin/clang++ -o x test.cpp -std=c++20 -L/usr/local/opt/llvm/lib/c++ -static-libstdc++ -I/usr/local/opt/llvm/include -L/usr/local/opt/llvm/lib
 
