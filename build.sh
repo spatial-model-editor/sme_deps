a9ff5438b236735a20c67333b1a97b7d695ad439
Liam Keegan <liam@keegan.ch>
2023-08-14 15:21:08 +0200

    set clang, try to directly link to static libc++

--- build.sh
+++ build.sh
@@ -33,9 +33,11 @@ fi
 export DUNE_COPASI_USE_STATIC_DEPS=ON
 export CMAKE_INSTALL_PREFIX=$DEPSDIR
 export MAKE_OPTIONS="-j2 VERBOSE=1"
-export CMAKE_CXX_FLAGS="'-fvisibility=hidden -D_GLIBCXX_USE_TBB_PAR_BACKEND=0 -L/usr/local/opt/llvm/lib/c++ -Wl,-rpath,/usr/local/opt/llvm/lib/c++'"
+export CMAKE_CXX_FLAGS="'-fvisibility=hidden -D_GLIBCXX_USE_TBB_PAR_BACKEND=0 -L/usr/local/opt/llvm/lib/c++ -static-libstdc++'"
 export DUNE_PDELAB_ENABLE_TRACING=OFF
 export DUNE_COPASI_USE_PARAFIELDS=OFF
+export CC=/usr/local/opt/llvm/bin/clang
+export CXX=/usr/local/opt/llvm/bin/clang++
 if [[ $MSYSTEM ]]; then
     # on windows add flags to support large object files
     # https://stackoverflow.com/questions/16596876/object-file-has-too-many-sections
